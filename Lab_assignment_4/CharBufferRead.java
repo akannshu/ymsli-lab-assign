8ce0bd485c46f5032299988f7c9b8e0b4baf7ac7
Akanshu <akanshuraj@gmail.com>
2021-01-22 22:46:37 +0530

    Add files via upload

--- Lab_assignment_4/CharBufferRead.java
+++ Lab_assignment_4/CharBufferRead.java
@@ -9,7 +9,22 @@ public class CharBufferRead {
 			BufferedWriter fileWriting = new BufferedWriter(new FileWriter(new File("C:\\Users\\ve00ym232\\Documents\\workspace-spring-tool-suite-4-4.9.0.RELEASE\\ymsli\\bin\\lab_assignment_4\\result.txt")));
 			fileWriting.write(fileReader.readLine());
 			
-			System.out.println();
+			fileWriting.close();
+			fileReader.close();
+			
+			System.out.println("File written");
+			
+			BufferedReader readFile = new BufferedReader(new FileReader("C:\\Users\\ve00ym232\\Documents\\workspace-spring-tool-suite-4-4.9.0.RELEASE\\ymsli\\bin\\lab_assignment_4\\result.txt"));
+			
+			String data;
+			
+			while(true) {
+				data = readFile.readLine();
+				if(data == null) break;
+				System.out.println(data);
+			}
+			
+			readFile.close();
 			
 		} catch(IOException ex) {
 			System.out.println("Some issue occured");
